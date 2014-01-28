732563b5322b7a06d3714b81c13c2674c35e57bd
Jeremy Rossi <jeremy@jeremyrossi.com>
2014-01-28 21:25:00 +0000

    Reverting by hand some items so that rhings are clean for most users

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 sys.path.append(".")
+sys.path.append("docs")
 extensions = [
         'sphinx.ext.autodoc', 
         'sphinx.ext.intersphinx', 
