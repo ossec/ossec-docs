f96b60e844bffbb605dfccbf7050f9181637eb2a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-25 23:57:18 +0800

    Delete _ext directory

--- _ext/xml_domain.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def setup(app):
-    app.add_object_type('element', 'xml', '%s')
-    app.add_object_type('attribute', 'xml', 'pair: %s; Attribute')
-    app.add_object_type('intopt', 'OS','%s')
-   
